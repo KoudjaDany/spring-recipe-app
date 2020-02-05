cf7652fdcc95804a0328764a5923cc84ab5784df
Dany Koudja <koudjadany@gmail.com>
2020-02-05 17:47:26 +0100

    Initialize the database with Spring. closes #7.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO category (category_name) VALUES ('American');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Mexican');
+INSERT INTO category (category_name) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (name) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (name) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (name) VALUES ('Cup');
+INSERT INTO unit_of_measure (name) VALUES ('Pinch');
+INSERT INTO unit_of_measure (name) VALUES ('Dunce');
+INSERT INTO unit_of_measure (name) VALUES ('g');
+INSERT INTO unit_of_measure (name) VALUES ('kg');
+
