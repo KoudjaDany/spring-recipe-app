29d0be0df1847dc2ec99511bbf15368c43ace284
Dany Koudja <koudjadany@gmail.com>
2020-02-05 16:32:38 +0100

    Add UnitOfMeasure entity and relationships with Ingredient entity. closes #3.

--- src/main/java/com/ddf/training/springrecipeapp/domain/Ingredient.java
+++ src/main/java/com/ddf/training/springrecipeapp/domain/Ingredient.java
@@ -11,8 +11,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-//TODO
-//    private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
