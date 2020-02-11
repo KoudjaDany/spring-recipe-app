769d523266dbd44046dcba0b8b83e47673fd5ddb
Dany Koudja <koudjadany@gmail.com>
2020-02-11 19:01:04 +0100

    Create a Unit Test for RecipeServiceImpl. Close #18.

---
+++ src/test/java/com/ddf/training/springrecipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.ddf.training.springrecipeapp.services;
+
+import com.ddf.training.springrecipeapp.domain.Recipe;
+import com.ddf.training.springrecipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeService recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void listAll() {
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.listAll();
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, atMost(2)).findAll();
+    }
+
+    @Test
+    public void getRecipe() {
+        Recipe recipe= new Recipe();
+        recipe.setId(1L);
+
+        when(recipeRepository.findById(recipe.getId())).thenReturn(Optional.of(recipe));
+
+        Recipe recipe1 = recipeService.getRecipe(1L);
+        assertEquals(recipe1.getId(), recipe.getId());
+    }
+}
