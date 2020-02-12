1a5b3e88cdc5bc05f5e1508f700652f378744737
Dany Koudja <koudjadany@gmail.com>
2020-02-12 11:40:04 +0100

    Add a MockMVC test for indexController. closes #21

--- src/test/java/com/ddf/training/springrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/ddf/training/springrecipeapp/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +16,9 @@ import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -30,6 +35,21 @@ public class IndexControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+        mockMvc.perform(get("details/"))
+                .andExpect(status().is4xxClientError());
+
+        mockMvc.perform(get("detail/"))
+                .andExpect(status().is4xxClientError());
     }
 
     @Test
