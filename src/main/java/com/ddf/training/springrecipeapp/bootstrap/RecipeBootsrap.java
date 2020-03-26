6d5719cbcf4039a749b29c79fdc492d775631b52
Dany Koudja <koudjadany@gmail.com>
2020-03-26 07:31:53 +0100

    Styling the Recipe details page.

--- src/main/java/com/ddf/training/springrecipeapp/bootstrap/RecipeBootsrap.java
+++ src/main/java/com/ddf/training/springrecipeapp/bootstrap/RecipeBootsrap.java
@@ -38,7 +38,7 @@ public class RecipeBootsrap implements ApplicationListener<ContextRefreshedEvent
     }
 
     private void loadSpicyGrilledChikenTacos(){
-        Recipe spicyGrilledChickenTacos= new Recipe();
+        Recipe spicyGrilledChickenTacos = new Recipe();
         spicyGrilledChickenTacos.setUrl("https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
         spicyGrilledChickenTacos.setCookTime(15);
         spicyGrilledChickenTacos.setPrepTime(20);
@@ -105,19 +105,19 @@ public class RecipeBootsrap implements ApplicationListener<ContextRefreshedEvent
                 .addAll(Arrays.asList(chillPowder, oregano, cumin, sugar, chicken, salt, oliveOil));
 
 
-        spicyGrilledChickenTacos.setDirection("<ol><li>Prepare a gas or charcoal grill for medium-high, direct heat.</li>\n" +
+        spicyGrilledChickenTacos.setDirection("Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
                 "\n" +
-                "<li>2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
+                "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
                 "\n" +
-                "Set aside to marinate while the grill heats and you prepare the rest of the toppings.</li>\n" +
+                "Set aside to marinate while the grill heats and you prepare the rest of the toppings.\n" +
                 "\n" +
-                "<li>3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes.</li>\n" +
+                "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes.\n" +
                 "\n" +
-                "<li>4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n" +
+                "4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n" +
                 "\n" +
-                "Wrap warmed tortillas in a tea towel to keep them warm until serving.</li>\n" +
+                "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n" +
                 "\n" +
-                "<li>5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges. </li></ol>");
+                "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges. ");
 
         recipeRepository.save(spicyGrilledChickenTacos);
         //log.debug("Spicy Grilled Chicken Tacos recipe : ", spicyGrilledChickenTacos);
@@ -207,17 +207,17 @@ public class RecipeBootsrap implements ApplicationListener<ContextRefreshedEvent
         perfectGuacamole.getIngredients()
                 .addAll(Arrays.asList(avocado, salt, onion, tomato, limeJuice, serrano, cilantro, blackPepper, radishes, tortillaChips));
 
-        perfectGuacamole.setDirection("<ol><li> Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.) Place in a bowl.</li>\n" +
+        perfectGuacamole.setDirection(" Cut the avocado, remove flesh: Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.) Place in a bowl.\n" +
                 "\n" +
-                "<li> Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)</li>\n" +
+                " Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)\n" +
                 "\n" +
-                "<li> Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
+                " Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
                 "\n" +
                 "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness. So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness.\n" +
                 "\n" +
-                "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste.</li>\n" +
+                "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with this recipe and adjust to your taste.\n" +
                 "\n" +
-                "<li> Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve.</li></ol>\n" +
+                " Serve: Serve immediately, or if making a few hours ahead, place plastic wrap on the surface of the guacamole and press down to cover it and to prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) Refrigerate until ready to serve.\n" +
                 "\n" +
                 "\n" +
                 " \n");
