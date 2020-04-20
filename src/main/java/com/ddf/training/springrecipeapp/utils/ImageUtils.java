69327f9402428cc1c73be1a68b327aceb23e51cb
Dany Koudja <koudjadany@gmail.com>
2020-04-20 20:45:16 +0200

    Implement an utilitary method to get an image from the system file onto the application. closes #50

--- src/main/java/com/ddf/training/springrecipeapp/utils/ImageUtils.java
+++ src/main/java/com/ddf/training/springrecipeapp/utils/ImageUtils.java
@@ -1,6 +1,12 @@
 package com.ddf.training.springrecipeapp.utils;
 
+
 import javax.validation.constraints.NotNull;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class ImageUtils {
 
@@ -28,6 +34,47 @@ public class ImageUtils {
         }
 
         return byteBoxed;
+    }
+
+    public static Byte[] getImage(String url) {
+        File file = new File(url);
+        Byte[] image = getBytesFrom(readAllBytes(file));
+        return image;
+    }
+
+
+    /**
+     * This method uses java.io.FileInputStream to read
+     * file content into a byte array
+     *
+     * @param file
+     * @return
+     */
+    private static byte[] readFileToByteArray(File file) {
+        FileInputStream fis = null;
+        // Creating a byte array using the length of the file
+        // file.length returns long which is cast to int
+        byte[] bArray = new byte[(int) file.length()];
+        try {
+            fis = new FileInputStream(file);
+            fis.read(bArray);
+            fis.close();
+
+        } catch (IOException ioExp) {
+            ioExp.printStackTrace();
+        }
+        return bArray;
+    }
+
+    private static byte[] readAllBytes(File file) {
+        Path path = file.toPath();
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 
 }
