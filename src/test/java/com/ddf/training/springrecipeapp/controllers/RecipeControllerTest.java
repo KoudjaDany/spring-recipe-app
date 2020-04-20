87df4328a99d0e5bb4553166ada2ba58a3114d32
Dany Koudja <koudjadany@gmail.com>
2020-04-20 21:00:16 +0200

    refactor RecipeControllerTest.

--- src/test/java/com/ddf/training/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/ddf/training/springrecipeapp/controllers/RecipeControllerTest.java
@@ -16,7 +16,8 @@ import org.springframework.ui.Model;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class RecipeControllerTest {
@@ -121,7 +122,7 @@ public class RecipeControllerTest {
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
         //When
-        mockMvc.perform(delete("/recipe/delete/1"))
+        mockMvc.perform(get("/recipe/delete/1"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/index"));
         //Then
